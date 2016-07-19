ff09274c53154a174fec1e6bc2a90aac3cb45098
Clifton Craig <clifton.craig@ge.com>
2016-07-19 14:30:33 -0700

    Adds set-pm-app.sh to set the app name along with the pm api endpoint.

---
+++ set-pm-app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PM_HOST=$(pm api | cut -d "/" -f 3)
+echo "Host identified as: ${PM_HOST}"
+echo "pm app is identified as: ${1}"
+
+VERIFY_SETTINGS=$(/usr/libexec/PlistBuddy "Settings.bundle/Root.plist" -c "print PreferenceSpecifiers:3:Title")
+if [ ${VERIFY_SETTINGS} != "Server" ]; then
+  echo "Settings plist structure different from expected. Unable to set pm host"
+  exit 1
+fi
+
+/usr/libexec/PlistBuddy "Settings.bundle/Root.plist" -c "set PreferenceSpecifiers:3:DefaultValue ${PM_HOST}"
+/usr/libexec/PlistBuddy "PredixMobileiOS/Info.plist" -c "set pmapp_name ${1}"
+echo "pm host set to: ${PM_HOST}"
+echo "pm app set to: ${1}"
